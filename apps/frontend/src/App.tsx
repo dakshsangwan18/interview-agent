60d1856b6411178929781a02ae922e655ae80dc6
Daksh <dakshop13@gmail.com>
2026-06-28 00:47:40 +0530

    removing linkedin as of now

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,10 +2,21 @@ import "../styles/globals.css";
 import { Input } from "./components/ui/input";
 import { Button } from "./components/ui/button";
 import { Form } from "./components/Form";
+import { useState } from "react";
+import { Interview } from "./components/Interview";
+import { Result } from "./components/Result";
+import { Toaster } from "sonner";
 
 export function App() {
+  const [page, setPage] = useState<"form" | "interview" | "results">("form");
+
   return (
-    <Form />
+    <div>
+      {page == "form" && <Form />}
+      {page == "interview" && <Interview />}
+      {page == "results" && <Result />}
+      <Toaster position="bottom-right"/>
+    </div>
   );
 }
 
--- apps/frontend/src/components/Form.tsx
+++ apps/frontend/src/components/Form.tsx
@@ -1,21 +1,39 @@
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useState } from "react";
+import { toast } from "sonner";
+import axios from "axios";
+import { BACKEND_URL } from "@/lib/config";
 
 export function Form() {
-    return <div className="h-screen w-screen flex justify-center items-center ">
+  const [github, setGithub] = useState("");
+
+  async function onSubmit() {
+    if (!github) {
+      //add more validation
+      toast("please provide valid github url");
+      return;
+    }
+    await axios.post(`${BACKEND_URL}/api/v1/pre-interview`, {
+      github,
+    });
+  }
+  return (
+    <div className="h-screen w-screen flex justify-center items-center ">
       <div>
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-      AI Interview kickstart
-    </h2>
+          AI Interview kickstart
+        </h2>
         <div className="p-2">
-          <Input placeholder="Linkedin URL" />
-        </div>
-        <div className="p-2">
-          <Input placeholder="GitHub URL" />
+          <Input
+            placeholder="GitHub URL"
+            onChange={(e) => setGithub(e.target.value)}
+          />
         </div>
         <div className=" flex justify-center p-4">
-          <Button> Start Interview</Button>
+          <Button onClick={onSubmit}> Start Interview</Button>
         </div>
       </div>
     </div>
+  );
 }
